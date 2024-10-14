2164b613ee22c8b2e67db2a5d885f1c33a320562
diego <diego.perini@gmail.com>
2024-10-14 05:43:00 +0200

    added WPT as a test component, included minimal usage instructions, needs more work

--- test/wpt/wpt.sh
+++ test/wpt/wpt.sh
@@ -2,6 +2,43 @@
 
 path=`dirname $0`
 
+#
+# NOTE: All this is preliminary work in progress.
+#
+# Both wpt and nwsapi repositories must be installed
+# side by side at the same folder level to have them
+# work together smoothly as intended for these tests.
+#
+# A working instance of PHP is needed to setup the
+# local server, then from the main nwsapi folder
+# execute the provided shell to setup for testing
+# using the following terminal command:
+#
+#      test/wpt/wpt.sh
+#
+# this will start the PHP local server and listen
+# for browser connections on port 8000, after this
+# you should be able to open the followeing URL:
+#
+#      http://localhost:8000
+#
+# After loading one of the tests, you willl have the
+# option to execute it using the browser internal QS
+# API or nwsapi alternatedly by pressing the reload
+# button (CTRL-R in browsers), or the RERUN button
+# found in each of the test pages itself.
+#
+# Each reload will change the environment used
+# from browser intrernal QS api to nwsapi 
+#
+# Always copy the main wpt testing component first
+# we need a local copy to be able to inject nwsapi
+#
+cp ../wpt/resources/testharness.js ./test/wpt/
+
+cp ./test/wpt/favicon.svg ../wpt/
+cp ./test/wpt/index.html ../wpt/
+
 # start an instance of the PHP internal httpd server
 # needed to interactively execute WPT tests in browsers
 
