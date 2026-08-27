f291d1e170685a25b1b59b0e2b5372bb64aa5522
Diego Perini <diego.perini@gmail.com>
2026-08-27 11:24:47 +0200

    updated to release 2.2.25 for npm publishing

--- src/nwsapi.js
+++ src/nwsapi.js
@@ -5,9 +5,9 @@
  * nwsapi.js - Fast CSS Selectors API Engine
  *
  * Author: Diego Perini <diego.perini at gmail com>
- * Version: 2.2.24
+ * Version: 2.2.25
  * Created: 20070722
- * Release: 20260709
+ * Release: 20260827
  *
  * License:
  *  https://javascript.nwbox.com/nwsapi/MIT-LICENSE
@@ -30,7 +30,7 @@
 
 })(this, function Factory(global, Export) {
 
-  var version = 'nwsapi-2.2.24',
+  var version = 'nwsapi-2.2.25',
 
   doc = global.document,
   root = doc.documentElement,
