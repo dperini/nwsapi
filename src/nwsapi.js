6791b7b4aff99202a6093d948a618269641e9463
c <a@b>
2026-09-04 13:54:35 -0400

    cache-limit

--- src/nwsapi.js
+++ src/nwsapi.js
@@ -216,7 +216,7 @@
     },
 
   // caching limit for compiled resolver functions
-  CACHE_LIMIT = 1000,
+  CACHE_LIMIT = 4096,
 
   // ES5 bounded LRU cache. It stores query plans (compiled resolvers),
   // never DOM result sets. A prefixed dictionary avoids user-key collisions
